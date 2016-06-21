b275175e7b1bdad00edb8e20c2f8c613925b8b10
gyril <cyril.gantzer@gmail.com>
2016-06-21 11:14:51 -0700

    boom

--- server.js
+++ server.js
@@ -1,32 +1,39 @@
-var express = require("express");
+var express = require('express');
 var app = express();
 var PythonShell = require('python-shell');
 var port = Number(process.env.PORT) || 3000;
 
+// needed to parse JSON data from client
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 
 app.use(express.static('public'));
 
-
-app.get('/trueskill', function (req, res) {
-/*    var team1 = tableau de la team 1
-    var team2 = tableau de la team 2
-    var resultat = score du match (1 vs 2)*/
-
-    PythonShell.run('main.py', options, function (err, results) {
-        if (err) throw err;
-        // results is an array consisting of messages collected during execution
-        console.log('results: %j', results);
-    });
-
-    res.send("kikou");
-
+var players_temp = require('./players.json');
+app.get('/playerList', function (req, res) {
+    // FIXME: load list from database instead
+    var players = players_temp;
+    res.send(players);
 });
 
-app.get('*', function (req, res) {
-    res.status(404).send('404');
+app.post('/trueskill', function (req, res) {
+    var team1 = req.body.teams['1'];
+    var team2 = req.body.teams['2'];
+    var scoreTeam1 = req.body.score['1'];
+    var scoreTeam2 = req.body.score['2'];
+
+    console.log(team1, team2, scoreTeam1, scoreTeam2);
+
+    // PythonShell.run('main.py', options, function (err, results) {
+    //     if (err) throw err;
+    //     // results is an array consisting of messages collected during execution
+    //     console.log('results: %j', results);
+    // });
+
+    res.send({"message": "OK"});
 });
 
-app.listen(port, function () {
-  console.log('Tagpro Trueskill app');
-});
+app.get('*', function (req, res) { res.status(404).send('The flag has been captured.'); });
+
+app.listen(port, function () { console.log('Tagpro Trueskill app running on port', port); });
