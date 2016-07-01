4d1642a5ee9406ba9b5608d6af8b3605fea72a1f
theogz <theogz@users.noreply.github.com>
2016-07-01 15:15:21 +0200

    modified init.r

--- init.r
+++ init.r
@@ -1,3 +1,5 @@
-list.of.packages <- c("plotly", "reshape2", "plyr", "RPostgreSQL")
 
-install.packages(new.packages, repos='https://pbil.univ-lyon1.fr/CRAN/')
+install.packages("plotly", repos='https://pbil.univ-lyon1.fr/CRAN/')
+install.packages("reshape2", repos='https://pbil.univ-lyon1.fr/CRAN/')
+install.packages("plyr", repos='https://pbil.univ-lyon1.fr/CRAN/')
+install.packages("RPostgreSQL", repos='https://pbil.univ-lyon1.fr/CRAN/')
