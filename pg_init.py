444621e750f29f129aac2816eb312988945a1b74
theogz <theogz@users.noreply.github.com>
2016-07-11 15:50:24 +0200

    drop tables before creating new ones

--- pg_init.py
+++ pg_init.py
@@ -28,6 +28,7 @@ conn = psycopg2.connect(os.environ['DATABASE_URL'])
 cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 # Create the table of players and the first players
+cur.execute("DROP TABLE players;")
 cur.execute("CREATE TABLE players (id serial PRIMARY KEY, name varchar, mmr integer default 2500, sigma integer default 833);")
 cur.execute("INSERT INTO players (name) VALUES (%s);", ["HashtagYolo"])
 cur.execute("INSERT INTO players (name) VALUES (%s);", ["R0xx0r"])
@@ -51,13 +52,14 @@ cur.execute("SELECT count(*) FROM players;")
 print '%d players added to database' % cur.fetchone()['count']
 
 # Create the table of matchs and the first match
+cur.execute("DROP TABLE matchs")
 cur.execute("CREATE TABLE matchs (id serial PRIMARY KEY, team1 integer[], team2 integer[], score1 integer, score2 integer, computed integer);")
-cur.execute("INSERT INTO matchs (team1, team2, score1, score2, computed) VALUES (%s, %s, %s, %s, %s);", [[1,2], [3,4], 5, 2, 0])
+# cur.execute("INSERT INTO matchs (team1, team2, score1, score2, computed) VALUES (%s, %s, %s, %s, %s);", [[1,2], [3,4], 5, 2, 0])
 
 # Make sure we have the match
-cur.execute("SELECT * FROM matchs;")
-match = cur.fetchone()
-print 'match ID %d: team %s vs team %s, score %d-%d' % (match['id'], match['team1'], match['team2'], match['score1'],  match['score2'])
+# cur.execute("SELECT * FROM matchs;")
+# match = cur.fetchone()
+# print 'match ID %d: team %s vs team %s, score %d-%d' % (match['id'], match['team1'], match['team2'], match['score1'],  match['score2'])
 
 conn.commit()
 
