4e7cb15bac61ec18fa3a1f97eb41bacad1da1c78
Theo Gantzer <theo.gantzer@gmail.com>
2017-09-28 11:00:30 +0200

    preparing python file for new ranking ups and downs

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ table_matchs = pd.read_sql(sql_matchs, conn, coerce_float=True, params=None)
 #We create the dictionary based on players' current rating :
 Caracteristiques = dict(zip(liste_joueurs, rating_joueurs))
 
+# joueurs_concernes = []
 
 #Loop that update ratings for every match played :
 for i in xrange(0,table_matchs.shape[0]):
@@ -42,7 +43,9 @@ for i in xrange(0,table_matchs.shape[0]):
         Ratings_team_2 = [Caracteristiques[x] for x in Joueurs_team_2]
         Victoire_1 = int((table_matchs.loc[i,"score1"]>table_matchs.loc[i,"score2"])+0)
         Victoire_2 = int((table_matchs.loc[i,"score1"]<table_matchs.loc[i,"score2"])+0)
-
+        
+        # joueurs_concernes+=Joueurs_team_1
+        # joueurs_concernes+=Joueurs_team_2
         #We "simulate" the match
         New_ratings_team_1, New_ratings_team_2 = ts.rate([Ratings_team_1, Ratings_team_2], ranks=[Victoire_2,Victoire_1])
 
@@ -64,8 +67,14 @@ confiance_joueurs = [int(x.sigma*100) for x in Caracteristiques.values()]
 
 table_joueurs_nv = pd.DataFrame({'id':liste_joueurs, 'name':name_joueurs, 'mmr':ratings_joueurs, 'sigma':confiance_joueurs})
 
+# Trouver un moyen de soit rendre joueurs_concernes unique
+# Soit slice la table_joueurs_nv par lensemble des joueurs_concernes
+
+#table_joueurs_ayant_joue = table_joueurs_nv[table_joueurs_nv["id"]]
+
 for i in xrange(0,table_joueurs_nv.shape[0]):
     cur.execute("UPDATE players SET mmr = %s, sigma=%s WHERE id = %s;", [int(table_joueurs_nv.loc[i,"mmr"]), int(table_joueurs_nv.loc[i,"sigma"]), int(table_joueurs_nv.loc[i,"id"])])
+#    cur.execute("INSERT INTO mmr_variations (player_id, mmr, sigma) VALUES (%s, %s, %s);", [int(table_joueurs_nv.loc[i,"id"]), int(table_joueurs_nv.loc[i,"mmr"]), int(table_joueurs_nv.loc[i,"sigma"])])
 
 for i in xrange(0, table_matchs.shape[0]):
     cur.execute("UPDATE matchs SET computed = %s WHERE id = %s;", [int(table_matchs.loc[i,"computed"]), int(table_matchs.loc[i,"id"])])
